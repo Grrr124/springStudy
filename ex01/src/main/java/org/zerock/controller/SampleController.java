f94bc079fd0515651ac2b3bbeef42fa026cd7ced
Grrr124 <52157199+Grrr124@users.noreply.github.com>
2019-12-17 19:52:12 +0900

    model, binder

--- ex01/src/main/java/org/zerock/controller/SampleController.java
+++ ex01/src/main/java/org/zerock/controller/SampleController.java
@@ -1,14 +1,26 @@
 package org.zerock.controller;
 
+import java.net.http.HttpHeaders;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.zerock.domain.SampleDTO;
+import org.zerock.domain.SampleDTOList;
+import org.zerock.domain.TodoDTO;
 
 import lombok.extern.log4j.Log4j;
 
@@ -73,5 +85,72 @@ public class SampleController {
 		log.info("array ids: " + Arrays.toString(ids));
 		
 		return "ex02Array";
+	}
+	
+	//http://localhost:8090/sample/ex02Bean?list%5B0%5D.name=aaa&list%5B1%5D.name=bbb
+	//객체를 리스트화
+	@GetMapping("/ex02Bean")
+	public String ex02Bean(SampleDTOList list) {
+		
+		log.info("list dtos: " + list);
+		
+		return "ex02Bean";
+	}
+	
+	//DataTimeFormat도 변환 가능
+	 @InitBinder 
+	 public void initBinder(WebDataBinder binder) { 
+	 SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd");
+	 binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, false)); }
+	 
+	
+	@GetMapping("/ex03")
+	public String ex03(TodoDTO todo) {
+		
+		log.info("todo : " + todo);
+		return "ex03";
+	}
+	
+	//int page만 명시하면 타입까지 전달 안돼.. 그래서 ModelAttribute를 통해서 타입 관계없이 model에 담아 전달
+	//Modle타입과 더불어서 RedirectAttributes 타입이 있는데 model과 같지만 일회성으로 데이터를 전달하는 용도로 사용
+	//rttr.addFlashAttribute("name", "AAA");
+	//rttr.addFlashAttribute("age", "10);
+	//return "redirect:/";
+	@GetMapping("/ex04")
+	public String ex04(SampleDTO dto, @ModelAttribute("page") int page) {
+		
+		log.info("dto : " + dto);
+		log.info("page : " + page);
+		
+		return "/sample/ex04";
+	}
+	
+	//servlet-context.xml 설정이 맞물려 URL 경로를 View로 처리하기 때문에 생기는 에러페이지 발생
+	@GetMapping("/ex05")
+	public void ex05() {
+		log.info("/ex05................");
+	}
+	
+	@GetMapping("/ex06")
+	public @ResponseBody SampleDTO ex06() {
+		log.info("/ex06.............");
+		SampleDTO dto = new SampleDTO();
+		dto.setAge(10);
+		dto.setName("홍길동");
+		
+		return dto;
+	}
+	
+	@GetMapping("/ex07")
+	public ResponseEntity<String> ex07() {
+		log.info("/ex07.................");
+		
+		// {"name": "홍길동"}
+		String msg = "{\"name\": \"홍길동\"}";
+		
+		org.springframework.http.HttpHeaders header = new org.springframework.http.HttpHeaders();
+		header.add("Content-Type", "application/json;charset=UTF-8");
+		
+		return new ResponseEntity<>(msg, header, HttpStatus.OK);
 	}
 }
